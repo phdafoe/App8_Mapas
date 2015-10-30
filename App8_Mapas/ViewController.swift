31dc1527efa9610d99cc9d5ae7e206f72d585bdf
phdafoe <phdafoe@gmail.com>
2015-10-30 17:18:07 +0100

    actualización 301015_17_18

--- App8_Mapas/ViewController.swift
+++ App8_Mapas/ViewController.swift
@@ -40,12 +40,64 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         myFirstMap.setRegion(region, animated: true)
         
         
+        // Fase 3 -> Añadir una chincheta
+        
+        /*let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Madrid"
+        annotation.subtitle = "Hey estamos Aqui!"
+        
+        myFirstMap.addAnnotation(annotation)*/
+        
+        // Fase 4 -> Gesto del Dedo
+        
+        let longGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "actionGesture:")
+        longGestureRecognizer.minimumPressDuration = 2
+        myFirstMap.addGestureRecognizer(longGestureRecognizer)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - DELEGATE
+    // Fase 2 -> Actualizacion de la localizacion con el delegado
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation : CLLocation = locations[0] as CLLocation
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta : CLLocationDegrees = 0.01
+        let longDelta : CLLocationDegrees = 0.01
+        
+        let location : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let span : MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        let region : MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        myFirstMap.setRegion(region, animated: true)
+        
+        
+    }
+    
+    
+    //MARK: - UTILS
+    // Fase 4 -> Metodo Auxuliares
+    func actionGesture(gestureRecognizer: UIGestureRecognizer){
+        
+        let touchPoint = gestureRecognizer.locationInView(self.myFirstMap)
+        let newCoordinate : CLLocationCoordinate2D = myFirstMap.convertPoint(touchPoint, toCoordinateFromView: self.myFirstMap)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinate
+        annotation.title = "Madrid"
+        annotation.subtitle = "Hey aqui estamos"
+        
+        myFirstMap.addAnnotation(annotation)
+        
     }
 
 
