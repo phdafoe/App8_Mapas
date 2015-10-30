31e564ecb12a307e11bad4312ed2b3028a044d81
phdafoe <phdafoe@gmail.com>
2015-10-30 17:02:25 +0100

    actualización 301015_17_02

--- App8_Mapas/ViewController.swift
+++ App8_Mapas/ViewController.swift
@@ -7,11 +7,39 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    //MARK: - VARIABLES LOCALES
+    var locationManager = CLLocationManager()
+    
+    //MARK: - IB
+    @IBOutlet weak var myFirstMap: MKMapView!
+    
+
+    //MARK: - LYFE APP
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Fase 1 ->
+        
+        let latitude : CLLocationDegrees = 40.4
+        let longitude : CLLocationDegrees = -3.7
+        let latDelta : CLLocationDegrees = 0.01
+        let longDelta : CLLocationDegrees = 0.01
+        let location : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        let region : MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        myFirstMap.setRegion(region, animated: true)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
